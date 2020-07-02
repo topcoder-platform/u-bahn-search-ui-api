1ce2d7dc74bfb27b35f3311e86ef25bdfd37533e
Mithun Kamath <callmekatootie@gmail.com>
2020-07-02 16:02:14 +0530

    Fix issue where upload would not post to bus api

--- config/default.js
+++ config/default.js
@@ -16,6 +16,7 @@ module.exports = {
   TOKEN_CACHE_TIME: process.env.TOKEN_CACHE_TIME,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+  AUTH0_PROXY_SERVER_URL: process.env.AUTH0_PROXY_SERVER_URL,
 
   BUSAPI_URL: process.env.BUSAPI_URL || 'https://api.topcoder-dev.com/v5',
 
